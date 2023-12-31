1145f59f10e0a2a8eb9873267c438acf478efdbd
tumy-g <e9085@g.maizuru-ct.ac.jp>
2023-12-31 21:27:05 +0900

    shファイルを追加

---
+++ Start-jetsonNano.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Start Jetson Nano"
+git clone https://github.com/JetsonHacksNano/installSwapfile.git
+./installSwapfile/installSwapfile.sh
+sudo apt update
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip3 install -U pip testresources setuptools==49.6.0
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+sudo pip3 install -U --no-deps numpy==1.19.4 future==0.18.2 mock==3.0.5 keras_preprocessing==1.1.2 keras_applications==1.0.8 gast==0.4.0 protobuf pybind11 cython pkgconfig
---
+++ fan-start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Start Move Fan!"
+
+sudo jetson_clocks --fan
