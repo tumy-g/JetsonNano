186c566e7b2703884ce7efd5cdaa28f660408443
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-04-23 13:51:46 +0900

    hahaha

--- try2/example-mediapipe.py
+++ try2/example-mediapipe.py
@@ -1,10 +1,39 @@
 import time
 
 import cv2
+import sys
 import mediapipe as mp
 
 video_source = "/dev/video0"  # Use a webcam
 # video_source = "test_video.mp4"  # Path to video file
+
+# GStreamer pipeline
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
 # Initialize MediaPipe Pose and Drawing utilities
 mp_pose = mp.solutions.pose
@@ -12,9 +41,13 @@ mp_drawing = mp.solutions.drawing_utils
 pose = mp_pose.Pose()
 
 # Open the video file
-cap = cv2.VideoCapture(video_source)
+cap = cv2.VideoCapture(gstreamer_pipeline(display_width=640, display_height=360), cv2.CAP_GSTREAMER)
 time.sleep(2)
 
+if not cap.isOpened():
+    print('Can not open camera.')
+    sys.exit()
+    
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
