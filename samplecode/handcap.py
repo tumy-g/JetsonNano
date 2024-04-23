66992f6eb1dd2f5363070930ce2e76439f29b9c1
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-04-23 15:57:36 +0900

    edit hand

--- samplecode/handcap.py
+++ samplecode/handcap.py
@@ -1,8 +1,11 @@
-import cv2
+import time
 import sys
+import cv2
 import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
-# GStreamer pipeline
+video_source = "/dev/video0"
 def gstreamer_pipeline(
     capture_width=1280,
     capture_height=720,
@@ -10,10 +13,25 @@ def gstreamer_pipeline(
     display_height=720,
     framerate=60,
     flip_method=0,
-):import cv2
-import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
 # 静止画像の場合：
 with mp_hands.Hands(
@@ -45,7 +63,9 @@ with mp_hands.Hands(
         '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
 
 # Webカメラ入力の場合：
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(gstreamer_pipeline(display_width=640, display_height=360), cv2.CAP_GSTREAMER)
+print("start camera")
+time.sleep(2)
 with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
@@ -72,101 +92,4 @@ with mp_hands.Hands(
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
-cap.release()
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
-# landmarkの繋がり表示用
-landmark_line_ids = [ 
-    (0, 1), (1, 5), (5, 9), (9, 13), (13, 17), (17, 0),  # 掌
-    (1, 2), (2, 3), (3, 4),         # 親指
-    (5, 6), (6, 7), (7, 8),         # 人差し指
-    (9, 10), (10, 11), (11, 12),    # 中指
-    (13, 14), (14, 15), (15, 16),   # 薬指
-    (17, 18), (18, 19), (19, 20),   # 小指
-]
-
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    max_num_hands=2,                # 最大検出数
-    min_detection_confidence=0.7,   # 検出信頼度
-    min_tracking_confidence=0.7     # 追跡信頼度
-)
-
-cap = cv2.VideoCapture(gstreamer_pipeline(display_width=640, display_height=360), cv2.CAP_GSTREAMER)   # カメラのID指定
-if cap.isOpened():
-    while True:
-        # カメラから画像取得
-        success, img = cap.read()
-        if not success:
-            continue
-        img = cv2.flip(img, 1)          # 画像を左右反転
-        img_h, img_w, _ = img.shape     # サイズ取得
-
-        # 検出処理の実行
-        results = hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        if results.multi_hand_landmarks:
-            # 検出した手の数分繰り返し
-            for h_id, hand_landmarks in enumerate(results.multi_hand_landmarks):
-
-                # landmarkの繋がりをlineで表示
-                for line_id in landmark_line_ids:
-                    # 1点目座標取得
-                    lm = hand_landmarks.landmark[line_id[0]]
-                    lm_pos1 = (int(lm.x * img_w), int(lm.y * img_h))
-                    # 2点目座標取得
-                    lm = hand_landmarks.landmark[line_id[1]]
-                    lm_pos2 = (int(lm.x * img_w), int(lm.y * img_h))
-                    # line描画
-                    cv2.line(img, lm_pos1, lm_pos2, (128, 0, 0), 1)
-
-                # landmarkをcircleで表示
-                z_list = [lm.z for lm in hand_landmarks.landmark]
-                z_min = min(z_list)
-                z_max = max(z_list)
-                for lm in hand_landmarks.landmark:
-                    lm_pos = (int(lm.x * img_w), int(lm.y * img_h))
-                    lm_z = int((lm.z - z_min) / (z_max - z_min) * 255)
-                    cv2.circle(img, lm_pos, 3, (255, lm_z, lm_z), -1)
-
-                # 検出情報をテキスト出力
-                # - テキスト情報を作成
-                hand_texts = []
-                for c_id, hand_class in enumerate(results.multi_handedness[h_id].classification):
-                    hand_texts.append("#%d-%d" % (h_id, c_id)) 
-                    hand_texts.append("- Index:%d" % (hand_class.index))
-                    hand_texts.append("- Label:%s" % (hand_class.label))
-                    hand_texts.append("- Score:%3.2f" % (hand_class.score * 100))
-                # - テキスト表示に必要な座標など準備
-                lm = hand_landmarks.landmark[0]
-                lm_x = int(lm.x * img_w) - 50
-                lm_y = int(lm.y * img_h) - 10
-                lm_c = (64, 0, 0)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                # - テキスト出力
-                for cnt, text in enumerate(hand_texts):
-                    cv2.putText(img, text, (lm_x, lm_y + 10 * cnt), font, 0.3, lm_c, 1)
-
-        # 画像の表示
-        cv2.imshow("MediaPipe Hands", img)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q') or key == ord('Q') or key == 0x1b:
-            break
-
 cap.release()
